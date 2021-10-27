5aff0e06bbb57366c4aef16df62eeefa8f46042b
evulet1 <elvirochkata@gmail.com>
2021-10-27 11:06:53 +0300

    decorators with parametrs

--- simple.py
+++ simple.py
@@ -4,30 +4,29 @@ user = {'username': 'jose123', 'access_level': 'admin'}
 
 def user_has_permission(func):
     @functools.wraps(func)
-    def secure_func():
+    def secure_func(panel):
         '''
         Hey
         :return:
         '''
         if user.get('access_level') == 'admin':
-            return func()
+            return func(panel)
     return secure_func
 
 @user_has_permission
-def my_function():
+def my_function(panel):
     '''
     Allows us to retrieve the password for the admin panel.
     :return:
     '''
-    return 'Password for admin panel is 1234.'
+    return f'Password for {panel} panel is 1234.'
 
 @user_has_permission
 def another():
-    pass
-
-my_secure_function = user_has_permission(my_function)
+    return 'hello'
 
 
-print(my_function.__name__)
-print(another.__name__)
+print(my_function('movies'))
+print(another('movies'))
+
 
