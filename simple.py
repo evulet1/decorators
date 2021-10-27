a0e3bc40000b1158cb77099b10891646fd56f147
evulet1 <elvirochkata@gmail.com>
2021-10-27 11:41:05 +0300

    parametrs

--- simple.py
+++ simple.py
@@ -1,32 +1,23 @@
 import functools
 
-user = {'username': 'jose123', 'access_level': 'admin'}
+user = {'username': 'jose123', 'access_level': 'user'}
 
-def user_has_permission(func):
-    @functools.wraps(func)
-    def secure_func(panel):
-        '''
-        Hey
-        :return:
-        '''
-        if user.get('access_level') == 'admin':
-            return func(panel)
-    return secure_func
 
-@user_has_permission
+def user_has_permission(access_level):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def secure_func(panel):
+            if user.get('access_level') == access_level:
+                return func(panel)
+
+        return secure_func
+
+    return my_decorator
+
+
+@user_has_permission('user')
 def my_function(panel):
-    '''
-    Allows us to retrieve the password for the admin panel.
-    :return:
-    '''
     return f'Password for {panel} panel is 1234.'
-
-@user_has_permission
-def another():
-    return 'hello'
 
 
 print(my_function('movies'))
-print(another('movies'))
-
-
